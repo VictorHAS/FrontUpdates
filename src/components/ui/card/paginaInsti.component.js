8a27244a5e8c4a3db5561c6e668741f2bcbdc8c4
VictorHAS <victorhermesalves@gmail.com>
2019-05-07 10:50:06 -0300

    update pagina de instituição

--- src/components/ui/card/paginaInsti.component.js
+++ src/components/ui/card/paginaInsti.component.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import List from "../List";
+
 export default class pagInstituição extends Component {
   state = {
-    newNome: "",
-    newEndereco: "",
+    inst: "",
+    logradouro: "",
+    cep: "59078970",
+    numero: "",
+    complemento: "",
+    uf: "",
+    localidade: "",
+    bairro: "",
     items: []
   };
 
@@ -14,6 +21,9 @@ export default class pagInstituição extends Component {
 
     });*/
   };
+  componentDidMount() {
+    this.searchWithCep();
+  }
   /* do back-end
   componentDidMount() {
     fetch('https://api/lists', {
@@ -68,18 +78,51 @@ export default class pagInstituição extends Component {
   onSubmit = event => {
     event.preventDefault();
     this.setState({
-      items: [
-        ...this.state.items,
-        this.state.newNome + ", " + this.state.newEndereco
-      ]
+      items: [...this.state.items, this.state.inst + ", " + this.state.cep]
       //Inst, Cidade, Estado
     });
   };
   handleNameChange = e => {
-    this.setState({ newNome: e.target.value });
+    this.setState({ nome: e.target.value });
   };
-  handleEnderecoChange = e => {
-    this.setState({ newEndereco: e.target.value });
+  handleCepChange = e => {
+    this.setState({ cep: e.target.value });
+  };
+  handleLogradouroChange = e => {
+    this.setState({ logradouro: e.target.value });
+  };
+  handleBairroChange = e => {
+    this.setState({ bairro: e.target.value });
+  };
+  handleNumeroChange = e => {
+    this.setState({ numero: e.target.value });
+  };
+  handleComplementoChange = e => {
+    this.setState({ complemento: e.target.value });
+  };
+  handleCidadeChange = e => {
+    this.setState({ cidade: e.target.value });
+  };
+  handleLocalidadeChange = e => {
+    this.setState({ cidade: e.target.value });
+  };
+  handleUfChange =e=>{
+    this.setState({uf: e.target.value});
+  }
+  searchWithCep = () => {
+    fetch(`https://viacep.com.br/ws/${this.state.cep}/json`, {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          logradouro: data.logradouro,
+          numero: data.complemento,
+          bairro: data.bairro,
+          uf: data.uf,
+          localidade: data.localidade
+        });
+      });
   };
   //Rua,cep,numero,complemento
   render() {
@@ -93,35 +136,104 @@ export default class pagInstituição extends Component {
               className="form-control"
               id="inputNome"
               placeholder="Nome da Instituição Ex: UFRN "
-              value={this.state.newNome}
-              onChange={this.handleNameChange}
+              value={this.state.nome}
+              onChange={this.handleNomeChange}
             />
           </div>
           <div className="row">
-            <div className="form-group col-6">
-              <label htmlFor="inputEndereco">Endereço</label>
+            <div className="form-group col-3">
+              <label htmlFor="inputEndereco">CEP</label>
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputEndereco"
+                  placeholder="Ex: 59078-970"
+                  value={this.state.cep}
+                  onChange={this.handleCepChange}
+                />
+                <span className="input-group-append">
+                  <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    onClick={this.searchWithCep}
+                  >
+                    <i className="fa fa-search" />
+                  </button>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="form-group col-5">
+              <label htmlFor="inputEndereco">Logradouro</label>
               <input
                 type="text"
                 className="form-control"
                 id="inputEndereco"
-                placeholder="Ex: rua cet, s/n, natal"
-                value={this.state.newEndereco}
-                onChange={this.handleEnderecoChange}
+                placeholder="Ex: Av Senador Salgado Filho"
+                value={this.state.logradouro}
+                onChange={this.handleLogradouroChange}
               />
             </div>
-             <div className="form-group col-6">
-              <label htmlFor="inputEndereco">Cep</label>
+            <div className="form-group col-2">
+              <label htmlFor="inputEndereco">Numero</label>
               <input
                 type="text"
                 className="form-control"
                 id="inputEndereco"
-                placeholder="Ex: 59000-00"
-                value={this.state.newEndereco}
-                onChange={this.handleEnderecoChange}
+                placeholder="Ex: 3000 ou s/n"
+                value={this.state.numero}
+                onChange={this.handleNumeroChange}
+              />
+            </div>
+            <div className="form-group col-5">
+              <label htmlFor="inputEndereco">Complemento</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputEndereco"
+                placeholder="Ex: 66 ou s/n"
+                value={this.state.complemento}
+                onChange={this.handleComplementoChange}
               />
             </div>
           </div>
-         
+          <div className="row">
+            <div className="form-group col-5">
+              <label htmlFor="inputEndereco">Bairro</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputEndereco"
+                placeholder="Ex: Lagoa Nova"
+                value={this.state.bairro}
+                onChange={this.handleBairroChange}
+              />
+            </div>
+            <div className="form-group col-5">
+              <label htmlFor="inputEndereco">Cidade</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputEndereco"
+                placeholder="Ex: Natal"
+                value={this.state.localidade}
+                onChange={this.handleLocalidadeChange}
+              />
+            </div>
+            <div className="form-group col-2">
+              <label htmlFor="inputEndereco">Estado</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputEndereco"
+                placeholder="Ex: RN"
+                value={this.state.uf}
+                onChange={this.handleUfChange}
+              />
+            </div>
+          </div>
           <button type="submit" className="btn btn-primary mb-3 btn-sm">
             Incluir
           </button>
