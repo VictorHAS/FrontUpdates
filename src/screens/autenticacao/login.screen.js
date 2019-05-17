152805c801bd67d7641121a05a739f6ad2de8c74
VictorHAS <victorhermesalves@gmail.com>
2019-05-16 22:56:26 -0300

    integração back/front pagina login

--- src/screens/autenticacao/login.screen.js
+++ src/screens/autenticacao/login.screen.js
@@ -5,31 +5,33 @@ import { Link, Redirect } from "react-router-dom";
 export default class LoginScreen extends Component {
   state = {
     redirect: false,
-    msg: ""
+    msg: "",
+    email: "",
+    passwd: ""
   };
-  login(event) {
+  login = event => {
     event.preventDefault();
     const requestInfo = {
       method: "POST",
       body: JSON.stringify({
-        email: this.login_email.value,
-        password: this.login_password.value,
-        redirect: false
+        email: this.state.email,
+        password: this.state.passwd
       }),
       headers: new Headers({
         "Content-type": "application/json"
       })
     };
-    fetch("http://localhost:3002/auth", requestInfo)
+    fetch("http://192.168.0.22:3001/auth/authenticate", requestInfo)
       .then(response => {
         if (response.ok) {
-          return response.text();
+          return response.text()
         } else {
           throw new Error("Invalid email or password");
         }
       })
       .then(token => {
         localStorage.setItem("auth-token", token);
+        console.log(token)
         this.setState({ redirect: true });
       })
       .catch(err => {
@@ -37,72 +39,22 @@ export default class LoginScreen extends Component {
         localStorage.removeItem("auth-token");
       });
   }
-  register(event) {
-    event.preventDefault();
-    var name = this.register_name.value;
-    var enrollment = this.register_enrollment.value;
-    var email = this.register_email.value;
-    var password = this.register_password.value;
-    var confirm_password = this.register_confirmpassword.value;
-
-    var regex = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-    if (name === "") {
-      this.setState({ msg: "Informe seu nome completo" });
-    } else if (enrollment === "") {
-      this.setState({ msg: "Informe sua matrícula" });
-    } else if (email === "" || !regex.test(email)) {
-      this.setState({ msg: "Informe um endereço de email válido" });
-    } else if (password === "") {
-      this.setState({ msg: "Informe uma senha" });
-    } else if (confirm_password === "") {
-      this.setState({ msg: "Informe uma confirmação de senha" });
-    } else if (password !== confirm_password) {
-      this.setState({ msg: "A senha e confirmação de senha não correspondem" });
-    } else {
-      const requestInfo = {
-        method: "POST",
-        body: JSON.stringify({
-          name: name,
-          enrollment: enrollment,
-          email: email,
-          password: password
-        }),
-        headers: new Headers({
-          "Content-type": "application/json"
-        })
-      };
-
-      fetch("http://localhost:3001/auth", requestInfo)
-        .then(response => {
-          if (response.ok) {
-            return response.text();
-          } else {
-            throw new Error("Failed to register");
-          }
-        })
-        .then(msg => {
-          this.registerform.reset();
-          this.setState({
-            msg: ""
-          });
-        })
-        .catch(err => {
-          this.setState({ msg: "Erro: matrícula ou email indisponível" });
-        });
-    }
-  }
   componentDidMount() {
     document.title = "Realizar login - Plataforma LOP";
   }
-
+  handleEmailChange = e => {
+    this.setState({ email: e.target.value });
+  };
+  handlePasswordChange = e => {
+    this.setState({ passwd: e.target.value });
+  };
   render() {
     if (this.state.redirect) {
       return <Redirect to="/sistema/aluno" />;
     }
     return (
       <TemplateAutenticacao>
-        <form className="card">
+        <form className="card" onSubmit={this.login}>
           <div className="card-body p-6">
             <div style={{ textAlign: "center" }}>
               <img
@@ -113,11 +65,14 @@ export default class LoginScreen extends Component {
             </div>
             <div className="card-title">Faça login na sua conta</div>
             <div className="form-group">
+              <span className="alert-danger">{this.state.msg}</span>
               <label className="form-label">Endereço de e-mail</label>
               <input
                 type="email"
                 className="form-control"
                 placeholder="Digite seu e-mail"
+                value={this.state.email}
+                onChange={this.handleEmailChange}
               />
             </div>
             <div className="form-group">
@@ -134,6 +89,8 @@ export default class LoginScreen extends Component {
                 type="password"
                 className="form-control"
                 placeholder="**********"
+                value={this.state.passwd}
+                onChange={this.handlePasswordChange}
               />
             </div>
             <div className="form-group">
